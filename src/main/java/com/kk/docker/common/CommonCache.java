e43710fb7a60ae5196e115e94af365edcc343414
craengqq.com <craeng@126.com>
2021-04-16 09:11:55 +0800

    燃果——袁朝科——高级Java开发工程师

---
+++ src/main/java/com/kk/docker/common/CommonCache.java
@@ -0,0 +1,22 @@
+package com.kk.docker.common;
+
+/**
+ * @Author yuanchaoke craeng@126.com
+ * @Description //TODO
+ * @Date  22:54 22:54
+ * @Param
+ * @return
+ **/
+public interface CommonCache {
+
+    /**
+     * 短网址缓存前缀
+     */
+    String SHORT_URL = "u:short_url:";
+
+    /**
+     * api访问ak_sk前缀key
+     */
+    String Ak_SK ="u:ak_sk:";
+
+}
